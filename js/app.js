5940d2d7d000118790c29629ca434942b835b3fe
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-19 14:55:22 +0200

    add js content

--- js/app.js
+++ js/app.js
@@ -14,18 +14,30 @@ const title = document.getElementById('my_psw');
 
 user.name = prompt('Scrivi il tuo nome');
 while (user.name == '' || user.name == null) {
+    if (user.name == null) {
+        title.innerHTML = 'Non è stato possibile generare una password, ricarica la pagina per riprovare';
+        throw new Error("L'utente ha annullato la procedura!");
+    }
     user.name = prompt('Scrivi il tuo nome - non puoi proseguire se non lo fai');
 }
 console.log(user.name);
 
 user.surName = prompt('Scrivi il tuo cognome');
 while (user.surName == '' || user.surName == null) {
+    if (user.surName == null) {
+        title.innerHTML = 'Non è stato possibile generare una password, ricarica la pagina per riprovare';
+        throw new Error("L'utente ha annullato la procedura!");
+    }
     user.surName = prompt('Scrivi il tuo cognome - non puoi proseguire se non lo fai');
 }
 console.log(user.surName);
 
 user.favColor = prompt('Qual è il tuo colore preferito?');
 while (user.favColor == '' || user.favColor == null) {
+    if (user.favColor == null) {
+        title.innerHTML = 'Non è stato possibile generare una password, ricarica la pagina per riprovare';
+        throw new Error("L'utente ha annullato la procedura!");
+    }
     user.favColor = prompt('Scrivi qual è il tuo colore preferito - non puoi proseguire se non lo fai');
 }
 console.log(user.favColor);
