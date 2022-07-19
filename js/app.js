96d45dbcbc520df8a2308538ae77c763561f2cb6
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-19 14:40:20 +0200

    minor js changes

--- js/app.js
+++ js/app.js
@@ -16,18 +16,18 @@ user.name = prompt('Scrivi il tuo nome');
 while (user.name == '' || user.name == null) {
     user.name = prompt('Scrivi il tuo nome - non puoi proseguire se non lo fai');
 }
-console.log(user.name)
+console.log(user.name);
 
 user.surName = prompt('Scrivi il tuo cognome');
 while (user.surName == '' || user.surName == null) {
     user.surName = prompt('Scrivi il tuo cognome - non puoi proseguire se non lo fai');
 }
-console.log(user.surName)
+console.log(user.surName);
 
 user.favColor = prompt('Qual è il tuo colore preferito?');
 while (user.favColor == '' || user.favColor == null) {
     user.favColor = prompt('Scrivi qual è il tuo colore preferito - non puoi proseguire se non lo fai');
 }
-console.log(user.favColor)
+console.log(user.favColor);
 
 title.innerHTML = 'Password consigliata ' + user.name + user.surName + user.favColor + Math.floor(Math.random() * 100);
